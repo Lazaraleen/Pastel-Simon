7b4a06c2a9fe66b8141603200348fbba623c3d82
Myriam JAYLE <myriamjayle@gmail.com>
2023-11-04 12:38:19 +0100

    feu fini, avec pop up en cas de victoire ou perdu

--- app.js
+++ app.js
@@ -53,42 +53,88 @@ class Simon {
     }
 
     getRandomColor() {
-
+        return Math.floor(Math.random() * 4);
     }
 
     // exécute une fonction lorsqu'on clique sur un bouton
     buttonClick(value) {
-
+        !this.blockedButtons && this.validateChosenColor(value);
     }
 
     // valide si le bouton sélection correspond à celui de la séquence
     validateChosenColor(value) {
-
+        if (this.sequence[this.userPosition] === value) {
+            this.buttonSounds[value].play();
+            if (this.round === this.userPosition) {
+                this.updateRound(this.round + 1);
+                this.speed /= 1.02;
+                this.isGameOver();
+            } else {
+                this.userPosition++;
+            }
+        } else {
+            this.gameLost();
+        }
     }
 
     // vérifie si le jeu n'est pas fini
     isGameOver() {
-
+        if (this.round === this.totalRounds) {
+            this.gameWon();
+        } else {
+            this.userPosition = 0;
+            this.showSequence();
+        };
     }
 
     //Montre la séquence à reproduire
     showSequence() {
-
+        this.blockedButtons = true;
+        let sequenceIndex = 0;
+        let timer = setInterval(() => {
+            const button = this.buttons[this.sequence[sequenceIndex]];
+            this.buttonSounds[this.sequence[sequenceIndex]].play();
+            this.toggleButtonStyle(button);
+            setTimeout(() => this.toggleButtonStyle(button), this.speed /2);
+            sequenceIndex++;
+            if (sequenceIndex > this.round) {
+                this.blockedButtons = false;
+                clearInterval(timer);
+            }
+        }, this.speed);
     }
 
     // colore les boutons lorsque l'ordinateur montre la séquence
     toggleButtonStyle(button) {
-
+        button.classList.toggle('active');
     }
 
     // actualise Simon lorsque le joueur perd
     gameLost() {
-
+        this.errorSound.play();
+        Swal.fire({
+            position: 'center',
+            icon: 'error',
+            title: 'Tu as perdu',
+            showConfirmButton: true,
+        })
+        this.display.startButton.disabled = false;
+        this.blockedButtons = true;
     }
 
     // montre l'animation de victoire et actualise Simon lorsque le joueur gagne
     gameWon() {
-        
+        this.display.startButton.disabled = false;
+        this.blockedButtons = true;
+        this.buttons.forEach(element => {
+            element.classList.add('winner');
+        });
+        Swal.fire({
+            position: 'center',
+            icon: 'success',
+            title: 'Tu as gagné !!!',
+            showConfirmButton: true,
+        })
     }
 }
 
